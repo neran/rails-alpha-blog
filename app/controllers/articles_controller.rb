1c8ca12bb0cd790cb7ab33bcbbc42b1d153fa147
Neran <neran@onclick-ltd.com>
2017-04-16 21:33:46 +0000

    add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,34 @@
 class ArticlesController < ApplicationController
   
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
     @article = Article.new(article_params) 
     if @article.save
-      flash[:notice] = "Success"
+      flash[:notice] = "Create Success"
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Update Success"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
   
